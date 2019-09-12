a9e885acc283a30557f8da20698ab5db223de694
Thomas Skerbis <thomas.skerbis@mac.com>
2019-09-12 16:50:11 +0200

    Update rex_var_global_var.php
    https://github.com/FriendsOfREDAXO/global_settings/issues/5#issuecomment-530561495

--- lib/rex_var_global_var.php
+++ lib/rex_var_global_var.php
@@ -8,20 +8,21 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
-class rex_var_global_var extends rex_var 
+class rex_var_global_var extends rex_var
 {
-   protected function getOutput()        
-   {
-    $out = '';   
-       if ($this->hasArg('var') && $this->getArg('var')) {
-        if ($this->hasArg('empty') && $this->getArg('empty')=='1') {
-         $out = rex_global_settings::getDefaultValue($this->getArg('var'));    
-         }
-           else {
-               $out = rex_global_settings::getString($this->getArg('var'));
-           }
+    protected function getOutput()
+    {
+        $var = $this->getParsedArg('var', null, true);
+        if (null === $var) {
+            return false;
+        }
 
+        if ($this->getArg('empty') == '1') {
+            $method = 'getDefaultValue';
+        } else {
+            $method = 'getString';
+        }
+
+        return "rex_escape(rex_global_settings::$method($var))";
     }
-    return self::quote($out);
-   }
 }
