89c768f82a61ab986f83cb79b4fa6876b27c1ab4
eaCe <webmaster@slaps.de>
2021-10-08 17:30:07 +0200

    feat: remove 'rex_global_settings_parsedown', use 'rex_markdown' closes #3

--- pages/help.license.php
+++ pages/help.license.php
@@ -1,8 +1,10 @@
 <?php
-$file = rex_file::get($this->getPath('LICENSE.md'));
+[$Toc, $Content] = rex_markdown::factory()->parseWithToc(rex_file::require($this->getPath('LICENSE.md')), 2, 3, false);
 
-$parsedown = new rex_global_settings_parsedown();
-$content = $parsedown->text($file);
+$fragment = new rex_fragment();
+$fragment->setVar('content', $Content, false);
+$fragment->setVar('toc', $Toc, false);
+$content = $fragment->parse('core/page/docs.php');
 
 $fragment = new rex_fragment();
 $fragment->setVar('title', $this->i18n('help_license'));
