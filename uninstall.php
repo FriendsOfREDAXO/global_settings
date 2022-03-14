2a81708b1f297a0731748969bc53611fb61463fb
Thomas Skerbis <thomas.skerbis@mac.com>
2022-03-14 15:04:53 +0100

    uninstall.php ist nicht erforderlich
    fixes: https://github.com/FriendsOfREDAXO/global_settings/issues/43

--- uninstall.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-
-$curDir = __DIR__;
-//require_once $curDir . '/extensions/extension_cleanup.php';
-
-//rex_global_settings_cleanup(['force' => true]);
-
-rex_global_settings::deleteCache();
