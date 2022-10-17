217765d18c13fb12f1ad2c3aa247f9f263bc3301
eaCe <webmaster@slaps.de>
2022-10-17 19:25:49 +0200

    add some more unit tests

--- tests/global_settings_test.php
+++ tests/global_settings_test.php
@@ -29,3 +29,36 @@ test('expect the field already exists', function ()
     expect($returnValue)->toBeString();
     expect($returnValue)->toEqual(rex_i18n::msg('global_settings_field_error_unique_name'));
 });
+
+test('expect the cache to be cleared', function ()
+{
+    $returnValue = rex_global_settings::deleteCache();
+    expect($returnValue)->toBeTrue();
+});
+
+test('expect the value to be set', function ()
+{
+    rex_global_settings::init();
+    $returnValue = rex_global_settings::setValue(getName('test_name'), null, 'Hallo');
+    expect($returnValue)->toBeTrue();
+});
+
+test('expect to get the value', function ()
+{
+    rex_global_settings::init();
+    $returnValue = rex_global_settings::getValue(getName('test_name'), null);
+    expect($returnValue)->toEqual('Hallo');
+});
+
+test('expect the field to be deleted', function ()
+{
+    $returnValue = rex_global_settings_delete_field(getName('test_name'));
+    expect($returnValue)->toBeTrue();
+});
+
+test('expect the field does not exist', function ()
+{
+    $returnValue = rex_global_settings_delete_field(getName('test_name'));
+    expect($returnValue)->toBeString();
+    expect($returnValue)->toEqual(rex_i18n::msg('global_settings_field_error_invalid_name'));
+});
