1a57160cb0202c230c9c5b8b4b3a694acd743ba5
Wolfgang Bund <wb@dtp-net.de>
2023-01-18 10:07:36 +0100

    Update boot.php
    Die Anpassung hat bei mir die Warnmeldung
    Deprecated: str_replace(): Passing null to parameter #3 ($subject) of type array|string is deprecated ...
    unter PHP 8.1 beseitigt.
    Wenn die Warnmeldung durch einen Implementierungsfehler meinerseits verursacht wurde, kann der PR natürlich auch in den Abfalleimer.
    Vielleicht gibt es auch noch eine edlere Variante gegen die Meldung.

--- boot.php
+++ boot.php
@@ -48,7 +48,7 @@ if (rex::isBackend()) {
 		        $page = rex_be_controller::getPageObject('global_settings/settings');
 
 		        if (count(rex_clang::getAll(false)) > 1) {
-		            $clang_id = str_replace('clang', '', rex_be_controller::getCurrentPagePart(3));
+		            $clang_id = str_replace('clang', '', (string) rex_be_controller::getCurrentPagePart(3));
 		            $clangAll = \rex_clang::getAll();
 
 		            foreach ($clangAll as $id => $clang) {
